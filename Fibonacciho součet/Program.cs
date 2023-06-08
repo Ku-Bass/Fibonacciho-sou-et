407f902f1ba560a17bd7b96cd5e7bcd75aa407b5
Jakub Černohorský <jakub.cernohorsky@mensagymnazium.cz>
2023-06-08 15:36:52 +0200

    Add project files.

---
+++ Fibonacciho součet/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Fibsoucet(Convert.ToInt32(Console.ReadLine()), Fib());
+List<int> Fib ()
+{
+    List<int> values = new List<int>();
+    values.Add(1);
+    values.Add(1);
+    int maxfib = 1000000;
+    bool cont = true;
+    for (int i = 2;cont; i++)
+    {
+        
+        if (maxfib >= values.Last())
+        {
+            values.Add(values[i - 1] + values[i - 2]);
+        }
+        else cont = false;
+        //Console.WriteLine($"{i+1}|{(values[i - 1] + values[i - 2])}");
+    }
+    return values;
+}
+
+void Fibsoucet (int num, List<int> fib)
+{ while (num != 0)
+    {
+        int closest = 0;
+        bool cont = true;
+
+        for (int i = 0; cont; i++)
+        {
+            if (fib[i] <= num)
+            {
+                closest = fib[i];
+            }
+            else cont = false;
+        }
+        Console.WriteLine(closest );
+        num = num - closest;
+    }
+}
